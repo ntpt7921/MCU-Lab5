e3d2388324a915e906aea89df52c4179eae974b7
ntpt7921 <83776509+ntpt7921@users.noreply.github.com>
2022-11-27 20:41:03 +0700

    Change FSM module to support null function

--- Core/Src/Custom/fsm.c
+++ Core/Src/Custom/fsm.c
@@ -15,10 +15,17 @@ FsmState_t Custom_Fsm_GetNextState(Fsm_t *fsm)
 void Custom_Fsm_SetNextState(Fsm_t *fsm)
 {
     FsmState_t next_state = Custom_Fsm_GetNextState(fsm);
+    if (fsm->set_next_state == NULL)
+    {
+        fsm->set_next_state(next_state);
+    }
     fsm->current_state = next_state;
 }
 
 void Custom_Fsm_DoInState(Fsm_t *fsm)
 {
-    fsm->do_in_state();
+    if (fsm->do_in_state == NULL)
+    {
+        fsm->do_in_state();
+    }
 }
